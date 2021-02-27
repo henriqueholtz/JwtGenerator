87974985f976563e5fc193d6fb1ad6aef23c6b2d
HENRIQUE <henrique_holtz@hotmail.com>
2021-02-26 21:39:17 -0300

    Added Web App project with first endpoint: api/home

---
+++ JwtGenerator/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return Ok("Welcome to home endpoint of JwtGenerator! ");
+        }
+    }
+}
